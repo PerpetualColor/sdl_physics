4fa606700543a9c7d79e6ec585e43805c85e7838
Nolan Kornelsen <perpetualcolor@gmail.com>
2019-05-09 00:43:12 -0700

    remove lib/gl/target

--- lib/gl/target/debug/build/gl-dcc6791dec3b3a1e/build_script_build-dcc6791dec3b3a1e.d
+++
@@ -1,5 +0,0 @@
-/home/nolan/code/sdl_physics/lib/gl/target/debug/build/gl-dcc6791dec3b3a1e/build_script_build-dcc6791dec3b3a1e: build.rs
-
-/home/nolan/code/sdl_physics/lib/gl/target/debug/build/gl-dcc6791dec3b3a1e/build_script_build-dcc6791dec3b3a1e.d: build.rs
-
-build.rs:
--- lib/gl/target/debug/libgl.d
+++
@@ -1,1 +0,0 @@
-/home/nolan/code/sdl_physics/lib/gl/target/debug/libgl.rlib: /home/nolan/code/sdl_physics/lib/gl/build.rs /home/nolan/code/sdl_physics/lib/gl/src/lib.rs /home/nolan/code/sdl_physics/lib/gl/target/debug/build/gl-c81e0db6f5d5c531/out/bindings.rs
